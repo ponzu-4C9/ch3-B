1c0a705fbf6caeb1a271de015a320cc7193f4d03
ponzu-4C9 <baijingseiya@gmail.com>
2025-05-30 13:47:29 +0900

    金関連は完成

--- ch3-B.c
+++ ch3-B.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
-
+#include <limits.h>
 const int prices[5] = {160, 120, 130, 140, 150}; // 商品の価格：牛乳、麦茶、緑茶、コーヒー、紅茶
 const int money_kind[5] = {1000,500,100,50,10};
 #define numofMK (sizeof(money_kind)/sizeof(money_kind[0])) //money_kindの要素数
 
+// int配列を特定の値で初期化する関数
+void init_int_array(int* arr, int size, int value) {
+    for(int i = 0; i < size; i++) {
+        arr[i] = value;
+    }
+}
+// int配列の内容をprintfする関数
+#define print_array(arr, size) \
+    for(int i=0;i<size;i++) printf(#arr "[%d] = %d\n",i,arr[i]);
+
+
+//priceに合った出力する硬貨の枚数をresult配列によって表現する関数
+//envelopesにも変更を加える。
+//result＝結果の配列,envelopes=使用可能な硬貨の枚数の配列,price＝出力する金額
 int generate_ouput_money(int* result, int* envelopes,int price){
-    //出力する硬貨の枚数をresult配列によって表現する関数
-    //result＝結果の配列,envelopes=使用可能な硬貨の枚数の配列,price＝出力する金額
     int i = 0;
-    for(i = 0; i < numofMK;i++)
-        printf("%d,",envelopes[i]);
-        printf("\n");
     while (1){
         int tempcoin = price - money_kind[i];//引いてみる
+        int e = envelopes[i];
         if(tempcoin > 0 && envelopes[i] >= 1){
             envelopes[i]--;
             result[i]++;
-            result -= money_kind[i];
+            price -= money_kind[i];
         }else if (tempcoin == 0 && envelopes[i] >= 1)
         {
             envelopes[i]--;
             result[i]++;
+            price -= money_kind[i];
             break;
         }else if (i == numofMK-1 && envelopes[numofMK-1] == 0)//envelopes[numofMK-1]は十円玉の枚数
         {
-            return -1;
+            return -1;//硬貨が足りない
         }
         else if (i == numofMK){
-            printf("投入金額が足りません。\n");
-            break;
+            return -2;
         }else{//tempcoin < 0 
             i++;
         }
     }
-    return 0;
+    return price;
 }
 
 int main()
 {
-    int envelopes[5] = {1, 5, 5, 5, 5}; // 封筒(枚数)：千円札、五百円玉、百円玉、五十円玉、十円玉
+    int envelopes[5] = {0, 0, 1, 3, 1000}; // 封筒(枚数)：千円札、五百円玉、百円玉、五十円玉、十円玉
     int shelf[5] = {4, 4, 4, 4, 4}; // 商品棚：各飲料の在庫は4本
-    
+    print_array(envelopes,sizeof(envelopes)/sizeof(int))
     while (1)
     {
         int i;
@@ -48,7 +58,8 @@ int main()
         printf("0:牛乳160円, 1:麦茶120円, 2:緑茶130円, 3:コーヒー140円, 4:紅茶150円\n");
         printf("何を買う？ int>>");
         int choice;
-        scanf("%d", &choice);
+        //scanf("%d", &choice);
+        choice = 0;
         if (choice < 0 || choice > 4) {
             printf("無効な選択です。\n");
             return 1;
@@ -76,20 +87,27 @@ int main()
             sum += money_kind[i] * input_money[i];
         }
         printf("投入金額:%d円\n",sum);
+        print_array(envelopes,sizeof(envelopes)/sizeof(int))
         int chenge = sum - prices[choice];
         printf("おつり:%d円\n",chenge);
+        
 
         int ouput[5] = {0};
         int flg_lack = generate_ouput_money(ouput,envelopes,chenge);
-        printf("%d\n",flg_lack);
-
-        for(i = 0; i < sizeof(ouput)/sizeof(int);i++){
-            printf("ouput_money[%d]:%d\n",i,ouput[i]);
+        if (flg_lack == -1){
+            printf("硬貨が不足\n返金します\n");
+            for(i = 0; i < numofMK;i++){
+                envelopes[i] = buf_envelopes[i];//lack＝-1ということはenvelopes内の硬貨が足りないのでバックアップで復元
+            }
+            int temp[numofMK];
+            init_int_array(temp,numofMK,INT_MAX);
+            init_int_array(ouput,numofMK,0);
+            generate_ouput_money(ouput,temp,sum);//返金配列を生成
         }
 
-        for(i = 0; i < sizeof(envelopes)/sizeof(int);i++){
-            printf("envelopes[%d]:%d\n",i,envelopes[i]);
-        }
+
+        print_array(ouput,sizeof(ouput)/sizeof(int))
+        print_array(envelopes,sizeof(envelopes)/sizeof(int))
         
     }
     
